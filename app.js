5aa0c04398d25e1575c18ee1aab597a355deb23d
Robson Santos <robs.ssn@gmail.com>
2021-11-24 14:16:56 -0300

    implementado leitura de varios arquivos

--- app.js
+++ app.js
@@ -1,42 +1,63 @@
 fs = require("fs");
 const csv = require('csv-parser');
+let results = {};
+let report = '';
 
 try {
-  fs.readFileSync('data.csv');
+  scanDir();
 } catch (error) {
-  console.log('Não foi encontrado o arquivo data.csv na raiz');
+  console.log(error);
   return;
 }
 
-const results = [];
-fs.createReadStream('data.csv')
-  .pipe(csv({
-    mapHeaders: ({ header, index }) => header.toLowerCase()
-  },
-    { separator: '\t' }))
-  .on('data', (data) => results.push({
-    dateTime: data.datetime,
-    txhash: data.txhash,
-    bnbHistoricalUsd: data['historical $price/bnb'],
-    feeUsd: parseFloat(data['txnfee(usd)']),
-    feeBnb: parseFloat(data['txnfee(bnb)']),
-    valueInBnb: parseFloat(data['value_in(bnb)']),
-    valueOutBnb: parseFloat(data['value_out(bnb)']),
-    status: data.status,
-    method: data.method
-  }))
-  .on('end', () => {
-    consultDolarAndProcessReport();
+function scanDir(){
+  fs.readdirSync('exports').forEach(file => {
+    readCsv(file);
   });
+}
 
-function consultDolarAndProcessReport() {
+function readCsv(path){
+  let pathAtrib = path.split('.')[0];
+
+  Object.assign(results, {[pathAtrib]: []});
+
+  //console.log(results);
+
+  fs.createReadStream('exports/'+ path)
+    .pipe(csv({
+      mapHeaders: ({ header, index }) => header.toLowerCase()
+    },
+      { separator: '\t' }))
+    .on('data', (data) => results[pathAtrib].push({
+      dateTime: data.datetime,
+      txhash: data.txhash,
+      bnbHistoricalUsd: data['historical $price/bnb'],
+      feeUsd: parseFloat(data['txnfee(usd)']),
+      feeBnb: parseFloat(data['txnfee(bnb)']),
+      valueInBnb: parseFloat(data['value_in(bnb)']),
+      valueOutBnb: parseFloat(data['value_out(bnb)']),
+      status: data.status,
+      method: data.method
+    }))
+    .on('end', () => {
+      consultDolarAndProcessReport(path, pathAtrib);
+    });
+}
+
+function consultDolarAndProcessReport(path, pathAtrib) {
+
+  //CASO ENCONTRE ARQUIVO SEM TRANSAÇOES
+  if(results[pathAtrib].length < 1){
+    return;
+  }
+
   let totalFeeUsd = 0;
   let totalMovimentedUsd = 0;
   let totalTransferOutUsd = 0;
   let totalTransferInUsd = 0;
   let totalSwapUsd = 0;
 
-  results.map(r => {
+  results[pathAtrib].map(r => {
     //CALCULO DE TRANSFEFRENCIAS
     if (r.method === "Transfer") {
 
@@ -62,19 +83,20 @@ function consultDolarAndProcessReport() {
   });
 
   totalMovimentedUsd = totalTransferInUsd + totalSwapUsd + totalTransferOutUsd;
-  console.log(results);
 
-  let report = 'Data ' + results[0].dateTime
-    + '\nAté ' + results[results.length - 1].dateTime + ' \n'
-    + 'Quantidade de transações: ' + results.length + ' \n'
+  //console.log(results);
+
+  report += 'Arquivo: '+ path + ' \n'
+    + 'De: ' + results[pathAtrib][0].dateTime
+    + '\nAté: ' + results[pathAtrib][results[pathAtrib].length - 1].dateTime + ' \n'
+    + 'Quantidade de transações: ' + results[pathAtrib].length + ' \n'
     + 'Entradas: $' + totalTransferInUsd.toFixed(2) + ' \n'
     + 'Alienações: $' + totalSwapUsd.toFixed(2) + ' \n'
     + 'Saidas: $' + totalTransferOutUsd.toFixed(2) + ' \n'
     + 'Taxa: $' + totalFeeUsd.toFixed(2) + ' \n'
-    + 'Movimentado: $' + totalMovimentedUsd.toFixed(2);
+    + 'Movimentado: $' + totalMovimentedUsd.toFixed(2)+ ' \n\n';
 
-  console.log(report);
-  console.log('Análise completa, relatório gerado na raiz!');
+  console.log(report); 
 
   fs.writeFileSync('relatório.txt', report, (err) => {
     if (err) throw err;
